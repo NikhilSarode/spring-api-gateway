643a7c83aa28fd34c480a93f080da0e75ed1b28f
NikhilSarode <nikhilsarode2974@gmail.com>
2025-07-20 21:46:11 +0530

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    java
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    // Optional: For service discovery using Kubernetes
+    // implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-client")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+extra["springCloudVersion"] = "2023.0.1"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
